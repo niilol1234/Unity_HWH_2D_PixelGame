df97f5a5ea38d9f8be10742d295467c8a84c71a4
unknown <73327883+niilol1234@users.noreply.github.com>
2021-03-19 10:02:01 +0800

    03191001

--- PixelGame/Assets/script/Player.cs
+++ PixelGame/Assets/script/Player.cs
@@ -15,11 +15,62 @@ public class Player : MonoBehaviour
     // 布林值  bool true 是,false 否
     // 字串    string
     [Header("等級")]
-    [Tooltip("這是角色的等級")]
     public int lv = 1;
     [Header("移動速度"),Range(0,300)]
     public float speed = 10.5f;
+    [Header("角色是否死亡")]
     public bool isDead = false;
     [Tooltip("這是角色的名稱")]
     public string cName = "貓咪";
+    [Header("虛擬搖桿")]
+    public FixedJoystick joystick;
+
+    // 方法語法 Method - 儲存複雜的程式區塊或演算法
+    // 修飾詞 類型 名稱 () { 程式區塊或演算法 }
+    // void 無類型
+
+    /// <summary>
+    /// 移動
+    /// </summary>
+    private void Move()
+    {
+        print("移動");
+
+        float h = joystick.Horizontal;
+        print("水平:" + h);
+
+        float v = joystick.Vertical;
+        print("垂直:" + v);
+    }
+
+    private void Attack()
+    {
+
+    }
+
+    private void Hit()
+    {
+
+    }
+
+    private void Dead()
+    {
+
+    }
+
+    // 事件 - 特定時間會執行的方法
+    // 開始事件 : 播放後執行一次
+    private void Start()
+    {   
+           
+        Move();
+    }
+
+    //更新事件 : 大約一秒執行六十次 60FPS
+    private void Update()
+    {
+        // 呼叫方式
+        // 方法名稱();  
+        Move();
+    }
 }
