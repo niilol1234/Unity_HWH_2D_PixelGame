0e6067b54b6dde728385b47f49804a5a64c1749f
niilol1234 <73327883+niilol1234@users.noreply.github.com>
2021-03-26 10:06:23 +0800

    202103261006

--- PixelGame/Assets/script/CameraControl.cs
+++ PixelGame/Assets/script/CameraControl.cs
@@ -8,4 +8,30 @@ public class CameraControl : MonoBehaviour
     public Vector2 limitY = new Vector2(-5, 5);
     [Header("左右邊界")]
     public Vector2 limitX = new Vector2(-5, 5);
+    [Header("玩家")]
+    public Transform player;
+
+    private void Update()
+    {
+        TracK();
+    }
+
+    /// <summary>
+    /// 追蹤
+    /// </summary>
+    private void TracK()
+    {
+        Vector3 posCam = transform.position;  // 取得 攝影機座標
+        Vector3 posPla = player.position;     // 取得 玩家座標
+
+        posCam = Vector3.Lerp(posCam, posPla, 0.5f * speed * Time.deltaTime);
+        posCam.z = -10;
+
+        // Mathf.Clamp(要夾住的值，最小值，最大值)
+
+        posCam.x = Mathf.Clamp(posCam.x, limitX.x, limitX.y);
+        posCam.y = Mathf.Clamp(posCam.y, limitY.x, limitY.y);
+
+        transform.position = posCam;          // 更新攝影機的座標
+    }
 }
